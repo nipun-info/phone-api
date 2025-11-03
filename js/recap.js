ecdfe8978b14330227025b97f667d8e2dd0243ee
nipun shah <nipun.info.2020@gmail.com>
2025-11-03 22:04:52 +0600

    practice again with recap.js

--- js/recap.js
+++ js/recap.js
@@ -1,60 +1,60 @@
 const loadPhone = async (searchText, isShowAll) => {
-    // const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    const res = await fetch(url);
+    const res = await fetch(url)
     const data = await res.json();
-    const phones = data.data;
-    displayPhones(phones, isShowAll);
+    const phones = data.data
+    displayPhones(phones, isShowAll);  
 }
+
 
 const displayPhones = (phones, isShowAll) => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
-
-    // clear phone container cards before adding new cards
     phoneContainer.textContent = '';
 
-    // display show all button if there are more than 12 phones
     const showAllContainer = document.getElementById('show-all-container');
+
     if(phones.length > 12 && !isShowAll){
-        showAllContainer.classList.remove('hidden');
-    }
-    else {
+        showAllContainer.classList.remove('hidden')
+    }else 
+    {
         showAllContainer.classList.add('hidden');
     }
 
-    // display only first 12 phones if not show All
     if(!isShowAll){
-        phones = phones.slice(0,12);
+        phones = phones.slice(0, 12);
     }
 
     phones.forEach(phone => {
-        // console.log(phone);
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card border border-gray-300 shadow-lg`;
         phoneCard.innerHTML = `
             <figure class="px-10 pt-10">
-                <img src="${phone.image}" alt="Shoes"                class="rounded-xl" />
+                <img src="${phone.image}"
+                    alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title text-xl font-bold">${phone.phone_name}</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts
+                </p>
                 <div class="card-actions">
                     <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
                 </div>
             </div>
         `;
-        phoneContainer.appendChild(phoneCard);
-    });
+        phoneContainer.appendChild(phoneCard);        
+    })
     toggleLoadingSpinner(false);
 }
 
-const handleSearch = (isShowAll) => {   
-    const searchField = document.getElementById('search-field');
+const handleSearch = (isShowAll) => {
+    const searchText = document.getElementById('search-field').value;
     toggleLoadingSpinner(true)
-    const searchText = searchField.value;
-    // console.log(searchText)
-    loadPhone(searchText, isShowAll);   
+    loadPhone(searchText, isShowAll)  
+}
+
+const handleShowAll = () => {
+    handleSearch(true)
 }
 
 const toggleLoadingSpinner = (isLoading) => {
@@ -66,27 +66,13 @@ const toggleLoadingSpinner = (isLoading) => {
     }
 }
 
-// handle show all
-const handleShowAll = () =>{
-    handleSearch(true)
-}
-
 const handleShowDetails = async(id) => {
-    console.log('clicked show details', id);
-
-    // load single phone data
+    // console.log(id);
     const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
     const data = await res.json();
     const phone = data.data;
 
-    showPhoneDetails(phone);
-}
-
-const showPhoneDetails = (phone) => {
-    console.log(phone);    
-    
     const showDetailContainer = document.getElementById('show-detail-container');
-
     showDetailContainer.innerHTML = `
         <div class="">
             <div class=" flex items-center justify-center">
@@ -105,9 +91,8 @@ const showPhoneDetails = (phone) => {
         </div>
     `;
 
+    show_details_modal.showModal();  
 
-    // show the modal
-    show_details_modal.showModal();    
 }
 
-loadPhone(searchText='iphone');
+loadPhone(searchText = "13");
